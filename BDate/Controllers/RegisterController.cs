b774009995a156c8b4ff2088dcec17c552860952
Klims Klimovs <klims.klimovs@gmail.com>
2022-04-18 18:09:36 +0300

    reģistrācijas lapas melnraksts

---
+++ BDate/Controllers/RegisterController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BDate.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string Index(string name, string surname)
+        {
+            return name;
+        }
+    }
+}
