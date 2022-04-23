d8db5a9d47fed6308d337667b90487b79f2646b6
Rad <kalmanjudin2000@gmail.com>
2022-04-23 11:32:14 +0300

    Extended IdentityUser to ApplicationUser

--- BDate/Migrations/20220423081542_Extend_IdentityUser.Designer.cs
+++ BDate/Migrations/20220423081542_Extend_IdentityUser.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using BDate.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace BDate.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220423081542_Extend_IdentityUser")]
+    partial class Extend_IdentityUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
