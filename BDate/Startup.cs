bd7b850de1dbef01df172c2aef6df1d68eb59197
Rad <kalmanjudin2000@gmail.com>
2022-05-07 03:06:54 +0300

    Added new view for Profiles/Index endpoint

--- BDate/Startup.cs
+++ BDate/Startup.cs
@@ -76,6 +76,7 @@ namespace BDate
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
+            app.UseDefaultFiles();
             app.UseStaticFiles();
 
             app.UseRouting();
