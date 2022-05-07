e378b6232cf93360a0aa312b96abac445754afda
Rad <kalmanjudin2000@gmail.com>
2022-05-07 19:12:13 +0300

    Added view for Settings/Edit/{id} endpoint

--- BDate/Models/Setting.cs
+++ BDate/Models/Setting.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,7 +11,9 @@ namespace BDate.Models
     {
         [ForeignKey("Profile")]
         public string SettingId { get; set; }
+        [Display( Name = "Hide age")]
         public bool isHiddenAge { get; set; }
+        [Display(Name = "Hide Last name")]
         public bool isHiddenLastName { get; set; }
         public virtual Profile Profile { get; set; }
     }
