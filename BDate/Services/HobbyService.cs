d801040cefc40faad8f16c397954a5ccdd05ae19
Rad <kalmanjudin2000@gmail.com>
2022-05-08 14:33:39 +0300

    Added Service implementation for HobbiesController

---
+++ BDate/Services/HobbyService.cs
@@ -0,0 +1,53 @@
+﻿using BDate.Data;
+using BDate.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BDate.Services {
+    public class HobbyService : IHobbyService {
+        private readonly ApplicationDbContext _context;
+
+        public HobbyService(ApplicationDbContext context) {
+            _context = context;
+        }
+
+        public async Task Add(Hobby hobby) {
+            _context.Add(hobby);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(string id) {
+            var hobby = await _context.Hobbies.FindAsync(id);
+            _context.Hobbies.Remove(hobby);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Hobby>> GetAll() {
+            var result = await _context.Hobbies.ToListAsync();
+            return result;
+        }
+
+        public async Task<Hobby> GetById(string id) {
+            var result = await _context.Hobbies
+                .FirstOrDefaultAsync(m => m.HobbyId == id);
+            return result;
+        }
+
+        public async Task Update(Hobby hobby) {
+            //var result = await _context.Hobbies.FindAsync(id);
+            var result = _context.Update(hobby);
+            await _context.SaveChangesAsync();
+        }
+        public async Task<Hobby> Edit(string id) {
+            var result = await _context.Hobbies.FindAsync(id);
+            return result;
+        }
+
+        public bool HobbyExists(string id) {
+            return _context.Hobbies.Any(e => e.HobbyId == id);
+        }
+    }
+}
---
+++ BDate/Services/IHobbyService.cs
@@ -0,0 +1,17 @@
+﻿using BDate.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BDate.Services {
+    public interface IHobbyService {
+        Task<IEnumerable<Hobby>> GetAll();
+        Task<Hobby> GetById(string id);
+        Task<Hobby> Edit(string id);
+        Task Add(Hobby hobby);
+        Task Update(Hobby hobby);
+        Task Delete(string id);
+        bool HobbyExists(string id);
+    }
+}
